2c9003df5ab318ee292e141902fc567eb30e4192
s3baRr00 <sebastianrilo@gmail.com>
2019-09-16 18:02:47 -0300

    .

---
+++ Programacion/Archivos_Generico/index.php
@@ -0,0 +1,21 @@
+<?php
+
+    include_once './clases/persona.php';
+    include_once './clases/personasDao.php';
+
+    $request = ($_SERVER['REQUEST_METHOD']);
+
+    $dao = new GenericDao('../texto.txt');
+    switch($request){
+        case "POST" : 
+            if(isset($_POST["Nombre"]) && isset($_POST["Apellido"]) && isset($_POST["Legajo"])) {
+                $persona = new Persona($_POST["Nombre"], $_POST["Apellido"], $_POST["Legajo"]);
+                $dao->guardar($persona);
+            }
+            break;
+        case "GET" : 
+            echo $dao->listar();
+            break;
+        
+    }
+?>
